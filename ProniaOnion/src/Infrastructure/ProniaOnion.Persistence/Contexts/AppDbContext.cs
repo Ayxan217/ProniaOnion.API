9ab368bdbb0b8f342503dd799bf8da6d0c342cb1
ayxan217 <ayxanrm-bp217@code.edu.az>
2024-12-27 18:37:14 +0400

    AppDbContext added

---
+++ ProniaOnion/src/Infrastructure/ProniaOnion.Persistence/Contexts/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProniaOnion.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProniaOnion.Persistence.Contexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<ProductColor> ProductColors { get; set; }
+        public DbSet<Color> Colors { get; set; }
+       
+    }
+}
